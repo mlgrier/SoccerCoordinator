82f829946efa8c9f9dd62d4f3b73c110c0979026
Marco Grier <mlgrier@gmail.com>
2017-03-06 15:32:18 -0600

    Adding Info

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -2,11 +2,25 @@
 
 import UIKit
 
-var playerName = [String]()
-var playerHeight = [Int]()
-var playedBefore = [Bool]()
-var parentsName = [String]()
+
+/* Step 1: Creating a dictionary of all player information (Name, Height/Inches, Soccer Experience, Parent's Names */
 
 
-//Test again
-
+let player1: [String: Any] = ["name": "Joe Smith", "height": 42, "experienced": true, "guardianNames": "Jim and Jan Smith"]
+let player2: [String: Any] = ["name": "Jill Tanner", "height": 36, "experienced": true, "guardianNames": "Clara Tanner"]
+let player3: [String: Any] = ["name": "Bill Bon", "height": 43, "experienced": true, "guardianNames": "Sara and Jenny Bon"]
+let player4: [String: Any] = ["name": "Eva Gordon", "height": 45, "experienced": false, "guardianNames": "Wendy and Mike Gordon"]
+let player5: [String: Any] = ["name": "Matt Gill", "height": 40, "experienced": false, "guardianNames": "Charles and Sylvia Gill"]
+let player6: [String: Any] = ["name": "Kimmy Stein", "height": 41, "experienced": false, "guardianNames": "Bill and Hillary Stein"]
+let player7: [String: Any] = ["name": "Sammy adams", "height": 45, "experienced": false, "guardianNames": "Jeff Adams"]
+let player8: [String: Any] = ["name": "Karl Saygan", "height": 42, "experienced": true, "guardianNames": "Heather Bledsoe"]
+let player9: [String: Any] = ["name": "Suzane Greenberg", "height": 44, "experienced": true, "guardianNames": "Henrietta Dumas"]
+let player10: [String: Any] = ["name": "Sal Dali", "height": 40, "experienced": false, "guardianNames": "Gala Dali"]
+let player11: [String: Any] = ["name": "Joe Kavalier", "height": 39, "experienced": false, "guardianNames": "Sam and Elaine Kavalier"]
+let player12: [String: Any] = ["name": "Ben Finkelstein", "height": 45, "experienced": false, "guardianNames": "Wendy and Mike Gordon"]
+let player13: [String: Any] = []
+let player14: [String: Any] = []
+let player15: [String: Any] = []
+let player16: [String: Any] = []
+let player17: [String: Any] = []
+let player18: [String: Any] = []
